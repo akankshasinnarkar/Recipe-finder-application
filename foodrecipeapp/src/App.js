a2533bce010497d5356c12a98c1161e82bbb6704
akankshasinnarkar <akankshasinnarkar@gmail.com>
2022-07-05 18:03:56 +0530

    commit

--- foodrecipeapp/src/App.js
+++ foodrecipeapp/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Recipe from './Components/Recipe/Recipe';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Components/Login/Login';
+import SignUp from './Components/Signup/Signup';
+import Navigation from './Components/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-     <Recipe/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation/>
+                <div className="auth-wrapper">
+                  
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login/>} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+      
+    </Router>
   );
 }
 
